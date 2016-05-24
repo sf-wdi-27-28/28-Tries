f77029b3ee8dad140153c88079436fd8c1094d44
Justin Castilla <justincastilla@users.noreply.github.com>
2016-05-24 09:22:48 -0700

    Update Trie.js

--- Trie.js
+++ Trie.js
@@ -19,8 +19,42 @@ function Node(letter, valid){
 |******************************/
 
 Trie.prototype.add = function(word) {
-    // TODO: returns boolean true upon successfully inserting word into Trie
-    return false;
+    console.log("\nAdding the word '" + word + "'");
+    // protect from root damage
+    var current = root;
+    var match = (word[0] === current.letter);
+
+    // checks if word belongs in this Trie
+    if(!match) {
+      return false;
+    }
+    console.log("Skipping the node for   " + word[0]);
+    while(word.length>1){
+      //console.log(word);
+      // remove first letter from word (it already belongs)
+      word = word.slice(1,word.length);
+      var firstLetter = word[0];
+      // position of new fisrt letter
+      var position = firstLetter.charCodeAt(0) - 97;
+      // if position is empty, create a new node and dive in
+      if(current.letters[position] === 0){
+        // instantiate a next variable
+        console.log("Creating a new node for " + firstLetter);
+        var next = new Node(firstLetter);
+        // set the new node's letter to the root's current letter
+        // add next to the letters array in root
+        current.letters[position] = next;
+        // go 'down' the tree one level
+        current = current.letters[position];
+
+      // else position is filled, shift and dive in
+      } else {
+        console.log("Skipping the node for   " + word[0]);
+        // go 'down' the tree one level
+        current = current.letters[position];
+      }
+    }
+    return current.valid=true;
 };
 
 Trie.prototype.exists = function(word) {
@@ -30,11 +64,17 @@ Trie.prototype.exists = function(word) {
 
 /* New recursive structure: helper and recursive methods */
 Trie.prototype.printPrettyTrie = function() {
-  /* TODO: Print the Trie to the console using 'tabs' to delimit node level */
+    // protects the root from being smashed down to a leaf
+    var current = root;
+    console.log("\nPrinting DEPTH first:\n")
+    console.log(current.letter + " " + (current.valid ? 'valid' : ''));
+    print(current, '  ');
 };
 
 Trie.prototype.printTrieList = function(word) {
- /* TODO: Print a list of all nodes' keys in the list. order is not important */
+    var wordList = [];
+    // TODO: prints all of the words contained within the Trie
+    console.log("Total List Length: " + wordList.length)
 };
 
 /******************************|
@@ -64,6 +104,7 @@ var wordList = ['a', 'ace', 'aces', 'aced', 'acre', 'acres', 'act', 'acted', 'ac
 wordList.forEach(function(word) {
   trie.add(word);
 })
+
 // checks for existence of words previously added (all should be true)
 // var first = trie.exists('ace');
 // var second = trie.exists('acre');
@@ -73,4 +114,4 @@ wordList.forEach(function(word) {
 trie.printPrettyTrie();
 
 // // prints out full list of words
-// trie.printTrieList();Trie
+// trie.printTrieList();
